7f43e8bba6e641d42e8d9bcdc8c7fa425caced42
Joao Almeida <joalmeid@microsoft.com>
2019-11-11 16:23:39 +0000

    1st commit

---
+++ src/RingCentral.EngageDigital.SourceSdk.TestApp/ViewModels/UserViewModel.cs
@@ -0,0 +1,35 @@
+﻿using RingCentral.Sdk.TestApp.Helpers;
+using RingCentral.Sdk.TestApp.Models;
+using RingCentral.Sdk.TestApp.Views;
+
+namespace RingCentral.Sdk.TestApp.ViewModels
+{
+    public class UserViewModel : BaseViewModel
+    {
+        public MainPage MainPage { get; set; }
+
+        #region property CurrentUser
+        private User _currentUser;
+        public User CurrentUser
+        {
+            get { return _currentUser; }
+            set { SetProperty(ref _currentUser, value); }
+        }
+        #endregion
+
+        #region property IsNew
+        private bool _isNew;
+        public bool IsNew
+        {
+            get { return _isNew; }
+            set { SetProperty(ref _isNew, value); }
+        }
+        #endregion
+
+        public UserViewModel()
+        {
+            CurrentUser = new User(0);
+            IsNew = true;
+        }
+    }
+}
---
+++ src/RingCentral.EngageDigital.SourceSdk/Models/Formatting.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace RingCentral.EngageDigital.SourceSdk.Models
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum Formatting
+    {
+        [EnumMember(Value = "html")]
+        Html,
+        [EnumMember(Value = "text")]
+        Text,
+    }
+}
